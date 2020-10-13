6409a16aae8ee16ca62c1950c0a269b3c22fd1d8
dkaylee <dkayleeee2@gmail.com>
2020-10-13 18:07:27 +0900

    201013

---
+++ java_project/FirstJava/src/workSheet3/StringWork.java
@@ -0,0 +1,41 @@
+package workSheet3;
+
+public class StringWork {
+
+	public static void main(String[] args) {
+		/*
+		 * 문제 1. 다음 형태로 String 인스턴스를 생성. 
+		 * String str = “ABCDEFGHIJKLMN”; 
+		 * 그리고 이 문자열을 역순으로
+		 * 다시 출력하는 프로그램을 작성.
+		 */
+		
+		String str = "ABCDEFGHIJKLMN";
+		String result = "";
+		for(int i = str.length()-1; i>=0; i--) {
+			result += str.charAt(i);
+		}
+		System.out.println("result = " + result);
+	}
+	
+	/*
+	 * 문제 2. 다음 형태로 주민번호를 담고 있는
+	 * String 인스턴스를 하나 생성 
+	 * String str = “990929-1010123” 이
+	 * 문자열을 이용하여 중간에 삽입된 – 를 뺀 String 인스턴스를 생성.
+	 */
+	
+		String str1 = "990929-1010123";
+		String str2 = "";
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+
+}
