ab36b112b40a072263e7d1dcb68073bb81501f82
dkaylee <dkayleeee2@gmail.com>
2020-11-15 23:48:25 +0900

    ,

---
+++ java_project/MiniminiGame/src/miniminiGame/Comparable.java
@@ -0,0 +1,5 @@
+package miniminiGame;
+
+public interface Comparable {
+
+}
---
+++ java_project/MiniminiGame/src/miniminiGame/MyPage.java
@@ -0,0 +1,17 @@
+package miniminiGame;
+
+public class MyPage /*extends MemberInfoManager*/{
+	
+	List<MemberInfo> members;
+	
+	MemberInfoManager manager = MemberInfoManager.getInstance();
+	
+	public void myPage() {
+		
+		showInfor();
+	
+	}
+	
+	
+
+}
